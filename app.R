e9d76cbbb2b3301d8d378621c6acee56f68a4e43
TK <tkvang2@yahoo.com>
2021-11-22 15:47:29 -0600

    tried my best

--- app.R
+++ app.R
@@ -1,49 +1,76 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(reactable)
+library(shinyfilter)
+library(tidyverse)
 
-# Define UI for application that draws a histogram
+
+newdrinks<- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-26/cocktails.csv')
+
+
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  titlePanel("Cocktail Ingredients"),
+  sidebarLayout(
+    sidebarPanel(
+      width = 2,
+      
+      selectizeInput(inputId = "sel_manufacturer", label = "Type in your Ingredient",
+                     multiple = TRUE, options = list(onChange = event("ev_click")),
+                     choices = sort(unique(newdrinks$ingredient))),
+      selectizeInput(inputId = "sel_name", label = "Drink Name",
+                     multiple = TRUE, options = list(onChange = event("ev_click")),
+                     choices = sort(unique(newdrinks$drink))),
+      selectizeInput(inputId = "sel_measure", label = "Measure",
+                     multiple = TRUE, options = list(onChange = event("ev_click")),
+                     choices = sort(unique(newdrinks$measure))),
+    ),
+    mainPanel(
+      reactableOutput(outputId = "tbl_food")
     )
+  )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+server <- function(input, output, session) {
+  
+  r <- reactiveValues(mycars = newdrinks)
+  
+  define_filters(input,
+                 "tbl_food",
+                 c(sel_manufacturer = "Ingredient", 
+                   sel_name = "Name of Drink",
+                   sel_fuel = "Measure"), 
+                 newdrinks)
+  
+  
+  observeEvent(input$ev_click, {
+    r$mycars <- update_filters(input, session, "tbl_food")
+    update_tooltips("tbl_food", 
+                    session, 
+                    tooltip = TRUE, 
+                    title_avail = "Available is:", 
+                    title_nonavail = "Currently not available is:",
+                    popover_title = "My filters",
+                    max_avail = 10,
+                    max_nonavail = 10)
+  })
+  
+  
+  output$tbl_food <- renderReactable({
+    reactable(data = r$mycars,
+              filterable = TRUE,
+              rownames = FALSE,
+              selection = "multiple",
+              showPageSizeOptions = TRUE,
+              paginationType = "jump",
+              showSortable = TRUE,
+              highlight = TRUE,
+              resizable = TRUE,
+              rowStyle = list(cursor = "pointer"),
+              onClick = "select"
+    )
+  })
+  
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
---
+++ rsconnect.R
@@ -0,0 +1,10 @@
+install.packages('rsconnect')
+
+library(rsconnect)
+
+
+rsconnect::setAccountInfo(name='tkvang2', token='13F05A2427F9EF3AA7E3AEC591514CA4', secret='O3SpaUjf3DMzlWnS6pWz2Lj5D48b1mmuapQ+p/DV')
+
+
+library(rsconnect)
+rsconnect::deployApp('C:/Users/tkvan/Documents/11-interactive-graphics-tkvang2')
